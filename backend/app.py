60149ccdf5b4ef16fa42787f4595911d7da5aa7e
Steven Espinosa <stevenespinosa@steeleconsult.com>
2022-07-28 17:10:43 -0600

    parsing for main log

--- backend/app.py
+++ backend/app.py
@@ -7,9 +7,9 @@ from git_parse import GitLogParse
 
 class GitHandler:
     async def on_get(self, req, resp):
-        git_log_parse = GitLogParse("D:\\git\git-contrib\git-contrib.git")
+        git_log_parse = GitLogParse("D:\\git\\git-contrib")
         result = git_log_parse.parse()
-
+        print(result)
         resp.text = json.dumps(result)
         resp.status = falcon.HTTP_200
 
--- backend/git_parse.py
+++ backend/git_parse.py
@@ -1,18 +1,28 @@
-import string
-import typing
+import datetime
+
 from git import Repo
 
-
 class GitLogParse:
-    
-    def __init__(self, repo: string):
+    repo: str
+
+    def __init__(self, repo: str):
         self.repo = repo
 
     def parse(self):
         repo = Repo(self.repo)
         assert not repo.bare
 
-        print(repo)
-        resp = repo.log()
+        head = repo.head
+        main_ref = head.reference
+        log = main_ref.log()
 
+        resp = {}
+
+        for git_log in log:
+            actor = str(git_log.actor)
+            if actor not in resp.keys():
+                resp[actor] = []
+
+            fixed_date = datetime.datetime.fromtimestamp(git_log.time[0] - git_log.time[1]).isoformat()
+            resp[actor].append(fixed_date)
         return resp

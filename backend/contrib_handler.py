f42997177dbd89f34d810ca39c0bde551cfa873a
Steven Espinosa <stevenespinosa@steeleconsult.com>
2022-07-29 06:12:02 -0600

    format for d3

--- backend/contrib_handler.py
+++ backend/contrib_handler.py
@@ -17,15 +17,22 @@ class ContribHandler:
 
         days_of_year_count = [ start_date + datetime.timedelta(n) for n in range(int ((end_date - start_date).days))]
 
-        year_tuple = {}
+        aggregate_days_count = {}
         for day in days_of_year_count:
-            year_tuple[day.strftime("%Y-%m-%d")] = 0
+            aggregate_days_count[day.strftime("%Y-%m-%d")] = 0
         
         contributions = db.get_contributions_by_contributor(contributor, year)
 
         for contribution in contributions:
-            year_tuple[contribution[0].strftime("%Y-%m-%d")] = contribution[1]
+            aggregate_days_count[contribution[0].strftime("%Y-%m-%d")] = contribution[1]
+
+        result = []
+        for aggregate_day in aggregate_days_count:
+            result.append({
+                "date": aggregate_day,
+                "count": aggregate_days_count[aggregate_day]
+            })
 
         resp.status = falcon.HTTP_200
-        resp.text = json.dumps(year_tuple, default=str)
+        resp.text = json.dumps(result)
 

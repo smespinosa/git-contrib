caafb923a347c43cada594783d615b870461a67e
Steven Espinosa <stevenespinosa@steeleconsult.com>
2022-07-30 18:19:25 -0600

    week matrix from api

--- backend/contrib_handler.py
+++ backend/contrib_handler.py
@@ -1,5 +1,5 @@
 
-import datetime
+from datetime import date, timedelta
 import falcon
 import json
 from data_context import db_context as db
@@ -12,27 +12,43 @@ class ContribHandler:
 
         year: int = int(req.get_param("year"))
         
-        start_date = datetime.date(year, 1, 1)
-        end_date   = datetime.date(year + 1, 1, 1)
+        start_date = date(year, 1, 1)
+        end_date   = date(year + 1, 1, 1)
 
-        days_of_year_count = [ start_date + datetime.timedelta(n) for n in range(int ((end_date - start_date).days))]
+        days_of_year_count = [ start_date + timedelta(n) for n in range(int ((end_date - start_date).days))]
 
         aggregate_days_count = {}
         for day in days_of_year_count:
             aggregate_days_count[day.strftime("%Y-%m-%d")] = 0
         
-        contributions = db.get_contributions_by_contributor(contributor, year)
+        contributions = db.get_contributions_by_contributor(contributor, year)        
 
         for contribution in contributions:
             aggregate_days_count[contribution[0].strftime("%Y-%m-%d")] = contribution[1]
 
-        result = []
+        heatmap_result = [[None for x in range(7)] for y in range(53)]
+
+        for day in aggregate_days_count:
+            iso_date = date.fromisoformat(day)
+            week_of_year, day_of_week = self.get_week_and_day(iso_date)
+            heatmap_result[week_of_year][day_of_week] = aggregate_days_count[day]
+
+        response = {
+            "weeks": heatmap_result,
+            "contributions": []
+        }
+
         for aggregate_day in aggregate_days_count:
-            result.append({
+            response["contributions"].append({
                 "date": aggregate_day,
                 "count": aggregate_days_count[aggregate_day]
             })
 
-        resp.status = falcon.HTTP_200
-        resp.text = json.dumps(result)
 
+        resp.status = falcon.HTTP_200
+        resp.text = json.dumps(response)
+
+    def get_week_and_day(self, isodate: date):
+        day_of_week = int(isodate.strftime("%w"))
+        week_of_year = int(isodate.strftime("%U"))
+        return week_of_year, day_of_week
